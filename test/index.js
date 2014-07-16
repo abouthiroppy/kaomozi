c0b8d7d0d03eff9fd69fc9c83e942f5958354287
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2014-07-17 03:48:54 +0900

    records test

--- test/index.js
+++ test/index.js
@@ -1,25 +1,47 @@
 (function() {
   var kaomozi = require('../lib/kaomozi');
-
+  var assert = require('assert');
+  var jsonData;
   //Records
   ///////////////////////////////////////////////
   //{filter:"aa",since:1351567940,include_created_at:true}
-  kaomozi.records({},function(err,data){
-    console.log(data);
-  });
+  // kaomozi.records({},function(err,data){
+  //   console.log(data);
+  // });
   ///////////////////////////////////////////////
 
   //Record
   ///////////////////////////////////////////////
-  kaomozi.record(1111,function(err,data){
-    console.log(data);
-  });
+  // kaomozi.record(1111,function(err,data){
+  //   console.log(data);
+  // });
   ///////////////////////////////////////////////
 
   //Random
   ///////////////////////////////////////////////
-  kaomozi.random(function(err,data){
-    console.log(data);
-  });
+  // kaomozi.random(function(err,data){
+  //   console.log(data);
+  // });
   ///////////////////////////////////////////////
+
+  describe('API response test', function(){
+    it('Records', function(done){
+      kaomozi.records({},function(err,data){
+        jsonData = data;
+      });
+      setTimeout(function() {
+        // if(typeof jsonData === 'object'){
+        //   assert.ok(true);
+        // }
+        if(jsonData.length >= 100){
+          assert.ok(true);
+        }
+        else{
+          assert.ok(false);
+        }
+        done();
+      },4000);
+    });
+  });
+
 })();
